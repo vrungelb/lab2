4d4b5e46988596e40999be5bbd81d6b6e2ccaa2a
root <root@vrungelb.>
2025-06-09 23:09:13 +0500

    fifth commit

---
+++ build/CMakeFiles/queue.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/queue.dir/queue.c.o"
+  "CMakeFiles/queue.dir/queue.c.o.d"
+  "CMakeFiles/queue.dir/queue_array.c.o"
+  "CMakeFiles/queue.dir/queue_array.c.o.d"
+  "libqueue.pdb"
+  "libqueue.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
